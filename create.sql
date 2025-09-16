4c16e8a26e3ae199bef29ee50cd84432ab4bbaf0
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-09-16 22:06:30 +0300

    Bajarildi.

---
+++ create.sql
@@ -0,0 +1,14 @@
+INSERT INTO books (title, author, price)
+VALUES ('Clean Code', 'Robert C. Martin', 25.50);
+
+INSERT INTO books (title, author, price)
+VALUES ('The Pragmatic Programmer', 'Andrew Hunt', 30.00);
+
+INSERT INTO books (title, author, price)
+VALUES ('Python Crash Course', 'Eric Matthes', 22.99);
+
+INSERT INTO books (title, author, price)
+VALUES ('Atomic Habits', 'James Clear', 18.75);
+
+INSERT INTO books (title, author, price)
+VALUES ('Deep Work', 'Cal Newport', 27.45);
